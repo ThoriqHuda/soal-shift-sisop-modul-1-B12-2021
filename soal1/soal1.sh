efa87db36c0ec3dba22e57970b45b55d467072e1
han-tu <haann.m5@gmail.com>
2021-03-27 00:33:29 +0700

    File soal 1 dan 2 selesai

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,1 +1,88 @@
+#!/bin/bash
 
+file_used="/home/han/Downloads/syslog.log"
+
+# Sub Soal E
+declare -A map
+
+global_rematch(){
+    local line=$1 regex=$2
+    while [[ $line =~ $regex ]]; do
+        if [[ ${BASH_REMATCH[2]} = "Ticket doesn't exist" ]]
+        then
+         idx="Ticket doesn\'t exist"
+         ((map["$idx"]++))
+        else
+         ((map["${BASH_REMATCH[2]}"]++))
+        fi
+        line=${line#*"${BASH_REMATCH[0]}"}
+    done
+}
+
+global_rematch_again(){
+    local line=$1 regex=$2
+    while [[ $line =~ $regex ]]; do
+        echo "${BASH_REMATCH[3]},${BASH_REMATCH[1]}"
+        line=${line#*"${BASH_REMATCH[0]}"}
+    done
+}
+
+regex="(ERROR )(.*)(\ .*\))"
+regex2="(.*)(,)(.*)"
+
+while read -r l
+do
+ global_rematch "$l" "$regex"
+done < "$file_used"
+
+echo "Error,Count" > error_message.csv
+for i in "${!map[@]}"
+do
+ echo "${map[$i]},$i"
+done | sort -nr | while read -r ll
+do
+ global_rematch_again "$ll" "$regex2"
+done >> error_message.csv
+# --------END SUBSOAL D----------
+
+# Sub Soal E
+declare -A err
+declare -A info
+
+global_rematch2(){
+    local line=$1 regex=$2
+    while [[ $line =~ $regex ]]; do
+        if [[ ${BASH_REMATCH[1]} = "ERROR" ]]
+        then
+        ((err["${BASH_REMATCH[3]}"]++))
+        else
+        ((info["${BASH_REMATCH[3]}"]++))
+        fi
+        line=${line#*"${BASH_REMATCH[0]}"}
+    done
+}
+
+regex3="(ERROR|INFO)(.*\()(.*)(\))"
+
+while read -r lll
+do
+ global_rematch2 "$lll" "$regex3"
+done < "$file_used"
+
+echo "Username,INFO,ERROR" > user_statistic.csv
+for i in "${!err[@]}"
+do
+
+ if [[ ${err[$i]} -eq 0 ]]
+ then
+ ((err["$i"]=0))
+ fi
+
+ if [[ ${info[$i]} -eq 0 ]]
+ then
+ ((info["$i"]=0))
+ fi
+
+ echo "$i,${info[$i]},${err[$i]}"
+done | sort -n >> user_statistic.csv
+# --------END SUBSOAL E----------
--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -1,1 +1,72 @@
+#!/bin/bash
+awk '
+BEGIN {FS="\t"}
 
+# Sub Soal A
+{if (max_PP<=$21/($18-$21)*100) {
+max_PP=$21/($18-$21)*100
+max_idx=$1
+}}
+#---------------
+
+# Sub Soal B
+{if ($10=="Albuquerque" && $2~"2017") {iter[$7]++}}
+#---------------
+
+# Sub Soal C
+{
+if ($8=="Consumer") consumer+=1
+else if ($8=="Corporate") corporate+=1
+else if ($8=="Home Office") homeoffice+=1
+}
+#---------------
+
+# Sub Soal D
+{
+if ($13 == "West" || $13 == "East" || $13 == "South" || $13 == "Central")
+array[$13]+=$21
+}
+#---------------
+
+END {
+# Hasil Sub Soal A
+printf "Transaksi terakhir dengan profit percentage terbesar yaitu %d dengan persentase %.2f%%.\n", max_idx, max_PP
+#---------------
+
+# Hasil Sub Soal B
+printf "\nDaftar nama customer di Albuquerque pada tahun 2017 antara lain:\n"
+for (nama in iter) {
+   printf "%s\n", nama
+}
+#---------------
+
+# Hasil Sub Soal C
+printf "\nTipe segmen customer yang penjualannya paling sedikit adalah "
+if (consumer < corporate) {
+   if (consumer < homeoffice)
+      printf "%s dengan %d\n", "Customer", consumer
+   else
+      printf "%s dengan %d\n", "Home Office", homeoffice
+}
+else {
+   if (corporate < homeoffice)
+      printf "%s dengan %d\n", "Corporate", corporate
+   else
+      printf "%s dengan %d\n", "Home Office", homeoffice
+}
+#---------------
+
+# Hasil Sub Soal D
+min_profit=9999999
+for (i in array) {
+  if (min_profit > array[i]) {
+    min_profit = array[i]
+    min_reg=i
+  }
+}
+printf "\nWilayah bagian (region) yang memiliki total keuntungan (profit) yang paling sedikit adalah %s dengan total keuntungan %.2f\n", min_reg, min_profit
+#---------------
+
+}
+
+' /home/han/Downloads/Laporan-TokoShiSop.tsv > hasil.txt
